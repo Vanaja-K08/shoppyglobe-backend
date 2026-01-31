ebd3f766df08caed60e0a5885d902293fb2d4dba
vanaja <vanajakoppad@gmail.com>
2026-01-31 12:42:50 +0530

    connecting to database

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.json());
+
+// mongodb+srv://vanajakoppad_db_user:gAJE3rlKw6bjpnb9@cluster0.h52gie8.mongodb.net
+// MongoDB connection
+mongoose.connect('mongodb+srv://vanajakoppad_db_user:gAJE3rlKw6bjpnb9@cluster0.h52gie8.mongodb.net/shoppyglobe') //cloud - returns a promise
+.then(()=>{ console.log(`DB CONNECTED`) })
+.catch(()=>{ console.log(`DB NOT CONNECTED`) })
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
