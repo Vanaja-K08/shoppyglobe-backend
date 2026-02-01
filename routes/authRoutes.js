d4a65da23de3dd9229886bbffc778a943ae163f9
vanaja <vanajakoppad@gmail.com>
2026-02-01 19:40:28 +0530

    registraion

---
+++ routes/authRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const router = express.Router();
+
+// POST /register
+router.post("/register", async (req, res) => {
+  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const user = new User({
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  await user.save();
+  res.json({ message: "User registered" });
+});
