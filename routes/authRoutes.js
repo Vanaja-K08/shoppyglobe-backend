97302a46a0a40912bd5b5b720f4227f5a2bb468a
vanaja <vanajakoppad@gmail.com>
2026-02-01 20:12:54 +0530

    login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const router = express.Router();
 
@@ -15,5 +16,17 @@ router.post("/register", async (req, res) => {
   res.json({ message: "User registered" });
 });
 
+
+// POST /login
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ message: "Invalid credentials" });
+
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+});
 
 module.exports = router;
