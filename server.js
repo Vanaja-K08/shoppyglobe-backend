bac30fbe872f52e3267fe510b5b8fae03d7e6219
vanaja <vanajakoppad@gmail.com>
2026-02-02 18:52:53 +0530

    created env for secret key

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -14,9 +15,10 @@ app.use("/products", productRoutes);
 
 // mongodb+srv://vanajakoppad_db_user:gAJE3rlKw6bjpnb9@cluster0.h52gie8.mongodb.net
 // MongoDB connection
-mongoose.connect('mongodb+srv://vanajakoppad_db_user:gAJE3rlKw6bjpnb9@cluster0.h52gie8.mongodb.net/shoppyglobe') //cloud - returns a promise
-.then(()=>{ console.log(`DB CONNECTED`) })
-.catch(()=>{ console.log(`DB NOT CONNECTED`) })
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");  
