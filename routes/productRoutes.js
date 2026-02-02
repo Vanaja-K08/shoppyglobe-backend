908c736bf92fbe6e5dfb0d33c382747d7bd9208e
vanaja <vanajakoppad@gmail.com>
2026-02-02 18:19:13 +0530

    get by product id api

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -8,4 +8,11 @@ router.get("/", async (req, res) => {
   res.json(products);
 });
 
+// GET /products/:id
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(404).json({ message: "Product not found" });
+  res.json(product);
+});
+
 module.exports = router;
